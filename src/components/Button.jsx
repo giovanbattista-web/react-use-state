2d615b206d0a42d6715bc65cc9f2403058aa6133
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-09 07:55:53 +0200

    [UPD] STATO APPLICATO

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
+
 const Button = (props) => {
     const { language } = props;
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <>
             <div className="button">
                 <div>
-                    <button className="cursor_pointer">
+                    <button className="cursor_pointer" onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? "-" : "+"}
                         <h3>{language.title}</h3>
                     </button>
                 </div>
                 <div>
-                    {language.description}
+                    {isOpen && <div>{language.description}</div>}
                 </div>
 
             </div>
